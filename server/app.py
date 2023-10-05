a6de22fcd0e047e6cd620e49c72d17e8c0ffcfca
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-10-05 14:59:26 +0300

    final changes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from models import Patient, NextOfKin  # Import your models from models.py
 from flask_migrate import Migrate
+from models import Patient, NextOfKin, db  # Import your models from models.py
+
 app = Flask(__name__)
 
 # Configure your database URI here (e.g., SQLite)
@@ -10,40 +11,100 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional: Disable tracki
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 # Create a new patient and their next of kin
 @app.route('/patient', methods=['POST'])
 def create_patient_and_next_of_kin():
-    session = db.session
     data = request.json
 
     # Gather patient information
-    patient = Patient(fname=data['fname'], lname=data['lname'], password=data['password'],
-                      email=data['email'], phone_number=data['phone_number'])
-    session.add(patient)
+    patient = Patient(
+        fname=data['fname'], lname=data['lname'], password=data['password'],
+        email=data['email'], phone_number=data['phone_number']
+    )
 
     # Gather next of kin information
-    next_of_kin = NextOfKin(fname=data['next_of_kin']['fname'], lname=data['next_of_kin']['lname'],
-                            phone_number=data['next_of_kin']['phone_number'], patient=patient)
-    session.add(next_of_kin)
+    next_of_kin = NextOfKin(
+        fname=data['next_of_kin']['fname'], lname=data['next_of_kin']['lname'],
+        phone_number=data['next_of_kin']['phone_number'], patient=patient
+    )
 
-    session.commit()
-    session.close()
-    return jsonify({'message': 'Patient and Next of Kin created successfully', 'patient_id': patient.id}), 201
+    # Add and commit both patient and next_of_kin
+    try:
+        db.session.add(patient)
+        db.session.add(next_of_kin)
+        db.session.commit()
+        return jsonify({
+            'message': 'Patient and Next of Kin created successfully',
+            'patient': {
+                'id': patient.id,
+                'fname': patient.fname,
+                'lname': patient.lname,
+                'email': patient.email,
+                'phone_number': patient.phone_number
+            },
+            'next_of_kin': {
+                'id': next_of_kin.id,
+                'fname': next_of_kin.fname,
+                'lname': next_of_kin.lname,
+                'phone_number': next_of_kin.phone_number
+            }
+        }), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': 'An error occurred while creating the patient'}), 500
+    finally:
+        db.session.close()
 
-# Retrieve a patient by ID
+# Retrieve a patient and their next of kin by ID
 @app.route('/patient/<int:patient_id>', methods=['GET'])
 def get_patient(patient_id):
     patient = Patient.query.filter_by(id=patient_id).first()
+    next_of_kin = NextOfKin.query.filter_by(patient_id=patient_id).first()
+
     if patient:
-        return jsonify({'id': patient.id, 'fname': patient.fname, 'lname': patient.lname,
-                        'email': patient.email, 'phone_number': patient.phone_number}), 200
+        response = {
+            'patient': {
+                'id': patient.id,
+                'fname': patient.fname,
+                'lname': patient.lname,
+                'email': patient.email,
+                'phone_number': patient.phone_number
+            },
+            'next_of_kin': None
+        }
+
+        if next_of_kin:
+            response['next_of_kin'] = {
+                'id': next_of_kin.id,
+                'fname': next_of_kin.fname,
+                'lname': next_of_kin.lname,
+                'phone_number': next_of_kin.phone_number
+            }
+
+        return jsonify(response), 200
     else:
         return jsonify({'error': 'Patient not found'}), 404
+
+# Delete a patient and their next of kin by ID
+# Commented out as per request from Victor
+# @app.route('/patient/<int:patient_id>', methods=['DELETE'])
+# def delete_patient(patient_id):
+#     patient = Patient.query.get(patient_id)
+#
+#     if patient:
+#         next_of_kin = NextOfKin.query.filter_by(patient_id=patient_id).first()
+#         db.session.delete(patient)
+#         if next_of_kin:
+#             db.session.delete(next_of_kin)
+#         db.session.commit()
+#         return jsonify({'message': 'Patient and Next of Kin deleted successfully'}), 200
+#     else:
+#         return jsonify({'error': 'Patient not found'}), 404
 
 # Create tables based on the defined models
 with app.app_context():
     db.create_all()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/models.py
+++ server/models.py
@@ -1,24 +1,26 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
 
 Base = declarative_base()
+db = SQLAlchemy()
 
-class Patient(Base):
+class Patient(db.Model):
     __tablename__ = 'patient'
-    id = Column(Integer, primary_key=True)
-    fname = Column(String)
-    lname = Column(String)
-    password = Column(String(8))
-    email = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fname = Column(String(50))  
+    lname = Column(String(50))  
+    password = Column(String(8))  
+    email = Column(String(100))  
     phone_number = Column(Integer)
     next_of_kin = relationship("NextOfKin", back_populates="patient")
 
-class NextOfKin(Base):
+class NextOfKin(db.Model):
     __tablename__ = 'next_of_kin'
-    id = Column(Integer, primary_key=True)
-    fname = Column(String)
-    lname = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fname = Column(String(50))  
+    lname = Column(String(50))  
     phone_number = Column(Integer)
     patient_id = Column(Integer, ForeignKey('patient.id'))
     patient = relationship("Patient", back_populates="next_of_kin")
